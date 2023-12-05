ef1e1b3780dda703e27b10cc7e9be6320517ab78
Jebbis <92973916+Jebbis@users.noreply.github.com>
2023-12-05 15:53:10 +0200

    commit

--- app/(main)/_components/Navbar.tsx
+++ app/(main)/_components/Navbar.tsx
@@ -43,7 +43,6 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavBarProps) => {
 
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
-          <Title initialData={document} />
         </div>
       </nav>
       {document.isArchived && <Banner documentId={document._id} />}
